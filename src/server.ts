3aa6a52b2307e21b8452329785368c011483c358
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-10-31 03:54:04 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,24 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
     res.json({
         message: 'Hello, World!'
     })
 });
 
-app.post('/login',(req:Request,res:Response)=>{
-    const { username, password } = req.body;
+app.post('/login', (req: Request, res: Response) => {
+    const {username,password} = req.body
+    console.log({
+        username,
+        password
+    });
     res.json({
         message: `Logged in as ${username}`
     })
 })
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
