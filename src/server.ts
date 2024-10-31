42a26a7d7ca2adbd24f689b9a7af3ed11e65c2e3
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-11-01 01:36:14 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,10 @@ app.use(cors());
 app.get('/users', (req: Request, res: Response) => {
     try {
         const users = [
-            { id: 1, name: 'John Doe', email: 'john.doe@example.com',status:1 },
-            { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com',status:2 },
-            { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com',status:1 }
+            { id: 1, name: 'John Doe', email: 'john.doe@example.com', status: 1 },
+            { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', status: 1 },
+            { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com', status: 1 },
+            { id: 4, name: 'Thawitchai Boonsong', email: 'thawitchai.johnson@example.com', status: 1 }
         ];
         res.json(users);
     } catch (error) {
